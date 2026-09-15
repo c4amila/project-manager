4ef177eeca9da8144c1fc2b8c41b3bd57e079515
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-15 09:51:54 -0300

    feat(projeto): adiciona constantes

---
+++ src/main/java/com/c4amila/ProjectManager/infrastructure/controller/RestConstants.java
@@ -0,0 +1,8 @@
+package com.c4amila.ProjectManager.infrastructure.controller;
+
+public final class RestConstants {
+
+    public static final String PATH_PROJETOS = "/projetos";
+
+    private RestConstants(){}
+}
