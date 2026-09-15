27d9340add9a406d54051ed2e9697b5d6162d423
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-15 20:35:12 -0300

    feat(validation): adiciona Bean Validation em SalvarProjetoDTO

--- src/main/java/com/c4amila/ProjectManager/infrastructure/dto/SalvarProjetoDTO.java
+++ src/main/java/com/c4amila/ProjectManager/infrastructure/dto/SalvarProjetoDTO.java
@@ -1,15 +1,34 @@
 package com.c4amila.ProjectManager.infrastructure.dto;
 
 import com.c4amila.ProjectManager.domain.model.StatusProjeto;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class SalvarProjetoDTO {
+
+    @NotNull(message = "O nome deve ser obrigatório")
+    @Size(min = 1, max = 80)
     private final String nome;
+
+    @NotNull(message = "A descrição é obrigatória")
+    @Size(min = 1, max = 150)
     private final String descricao;
+
     private final StatusProjeto status;
+
+    @NotNull(message = "A data de início não pode ser vazia")
     private final LocalDate dataInicio;
+
+    @NotNull(message = "A data final não pode ser vazia")
     private final LocalDate dataFim;
+
+    @AssertTrue(message = "As datas não são consistentes")
+    private boolean isInitialDateBeforeFinalDate(){
+        return dataInicio.isBefore(dataFim);
+    }
 }
