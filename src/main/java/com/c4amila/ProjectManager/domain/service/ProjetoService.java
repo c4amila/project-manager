7d9eb285413e07e0f7d5e6279f566b293ad34639
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-15 09:55:05 -0300

    refact(projeto): corrige ProjetoService

--- src/main/java/com/c4amila/ProjectManager/domain/service/ProjetoService.java
+++ src/main/java/com/c4amila/ProjectManager/domain/service/ProjetoService.java
@@ -3,7 +3,7 @@ package com.c4amila.ProjectManager.domain.service;
 import com.c4amila.ProjectManager.domain.entity.Projeto;
 import com.c4amila.ProjectManager.domain.model.StatusProjeto;
 import com.c4amila.ProjectManager.domain.repository.ProjetoRepository;
-import com.c4amila.ProjectManager.infrastructure.dto.ProjetoDTO;
+import com.c4amila.ProjectManager.infrastructure.dto.SalvarProjetoDTO;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,14 +15,14 @@ public class ProjetoService {
     private final ProjetoRepository projetoRepository;
 
     @Transactional
-    public Projeto criarProjeto(ProjetoDTO projetoDTO){
+    public Projeto criarProjeto(SalvarProjetoDTO salvarProjetoDTO){
         Projeto projeto = Projeto
                 .builder()
-                .nome(projetoDTO.getNome())
-                .descricao(projetoDTO.getDescricao())
+                .nome(salvarProjetoDTO.getNome())
+                .descricao(salvarProjetoDTO.getDescricao())
                 .status(StatusProjeto.PENDENTE)
-                .dataInicio(projetoDTO.getDataInicio())
-                .dataFim(projetoDTO.getDataFim())
+                .dataInicio(salvarProjetoDTO.getDataInicio())
+                .dataFim(salvarProjetoDTO.getDataFim())
                 .build();
 
         projetoRepository.save(projeto);
